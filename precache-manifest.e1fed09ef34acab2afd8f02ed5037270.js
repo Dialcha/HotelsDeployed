6a09cb332946821c0bf95274ebc3b6a41433ca7b
DiegoTKD94 <diego.chavarriap@gmail.com>
2020-05-01 19:50:27 -0500

    sitio basico

---
+++ precache-manifest.e1fed09ef34acab2afd8f02ed5037270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54231029ee28db5cc34c5cfd2710d333",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8760015aaed85c817643",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "9edf64c347932d4f1123",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "8760015aaed85c817643",
+    "url": "/static/js/2.fb179d88.chunk.js"
+  },
+  {
+    "revision": "2efc0b494659ead92be8e25c4065b97c",
+    "url": "/static/js/2.fb179d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edf64c347932d4f1123",
+    "url": "/static/js/main.2b932ce4.chunk.js"
+  },
+  {
+    "revision": "fcc54b90dd4d35c0f31a",
+    "url": "/static/js/runtime-main.92837332.js"
+  }
+]);
